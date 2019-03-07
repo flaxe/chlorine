5904ba80a0e5c143d560c596e3e3903abc631177
flaxe <mail@flaxe.ru>
2019-03-07 09:37:36 +0300

    add expected

--- Chlorine/Util/Error.cs
+++ Chlorine/Util/Error.cs
@@ -7,27 +7,37 @@ namespace Chlorine
 		public readonly int Code;
 		public readonly string Message;
 
+		public readonly Exception Exception;
+
 		public Error(int code, string message)
 		{
 			Code = code;
 			Message = message;
+			Exception = null;
 		}
 
 		public Error(string message)
 		{
 			Code = -1;
 			Message = message;
+			Exception = null;
 		}
 
 		public Error(Exception exception)
 		{
 			Code = exception.HResult;
 			Message = exception.Message;
+			Exception = exception;
 		}
 
 		public override string ToString()
 		{
-			return $"{Message}({Code.ToString()})";
+			return $"{Message}({Code.ToString()}){(Exception != null ? $".{Environment.NewLine}{Exception.StackTrace}" : ".")}";
+		}
+
+		public static explicit operator Exception(Error error)
+		{
+			return error.Exception ?? new Exception(error.Message);
 		}
 	}
 }
